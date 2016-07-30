10f779cc5b692bd17f302b604d605d5958c8ec1b
Mark Filius <mark.filius@sports-med.com.au>
2016-07-30 22:08:06 +1000

    merge

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,4 @@
 'use strict';
-<<<<<<< HEAD
 const menuItems = require('./menuItems');
 const authentication = require('./authentication');
 const user = require('./user');
@@ -14,17 +13,5 @@ module.exports = function() {
   app.configure(authentication);
   app.configure(user);
   app.configure(menuItems);
-=======
-const authentication = require('./authentication');
-const user = require('./user');
-const mongoose = require('mongoose');
-module.exports = function() {
-  const app = this;
-  
-  mongoose.connect(app.get('mongodb'));
-  mongoose.Promise = global.Promise;
-  
-  app.configure(authentication);
-  app.configure(user);
->>>>>>> edf6a795e71073313b8ef4fd57f63d2ac508a4d6
+
 };
