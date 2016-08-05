af5081313997e3a88647e873ccce162012541f33
Mark Filius <mark.filius@sports-med.com.au>
2016-08-05 21:26:06 +1000

    M Menu items from db - not sorted yet

--- src/services/menuItems/index.js
+++ src/services/menuItems/index.js
@@ -9,12 +9,12 @@ module.exports = function() {
 
   const options = {
     Model: menuItems,
-    //paginate: false
-    $sort: {sequence: 1},
-    paginate: {
-        default: 25,
-        max: 11
-    }
+    paginate: false
+    //$sort: {sequence: 1}
+    //paginate: {
+    //    default: 25,
+    //    max: 11
+    //}
   };
 
   // Initialize our service with any options it requires
