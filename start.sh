0e6cfec9000711590014c00b6a3f579594fd96f3
Mark Filius <mark@filius.cc>
2016-08-08 12:12:04 +0000

    Homepage

--- start.sh
+++
@@ -1,7 +0,0 @@
-echo '--- Starting MongoDB'
-mongod --dbpath mongodb/db/ &
-
-echo '--- Starting app' 
-node src/index.js &
-
-
--- stop.sh
+++
@@ -1,8 +0,0 @@
-echo '--- Stopping MongoDB'
-killall mongod
-
-echo '--- Stopping Node.js' 
-killall node
-
-
-
