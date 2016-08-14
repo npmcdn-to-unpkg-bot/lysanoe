0e05b3e28e5618acb40972a8bbd77f53180df4be
Mark Filius <mark.filius@sports-med.com.au>
2016-08-14 11:48:00 +1000

    M added hook to filter and sort menuItems

--- src/services/menuItems/hooks/index.js
+++ src/services/menuItems/hooks/index.js
@@ -15,9 +15,27 @@ exports.before = {
 };
 
 exports.after = {
-  all: [],
-  find: [],
-  get: [],
+  all(hook) {
+      //console.log('afhookall:', hook.result[0]);  
+  },
+  find(hook) {
+      //console.log('afhookfind:', hook.result);
+      
+      // Filter only public menuItems
+      hook.result = hook.result.filter(function(menuItem) {
+          // The result object data is available through the _doc property !! 
+          return parseInt(menuItem._doc.allowedFor) <= 0;
+      });     
+      
+      // Sort on sequence
+      hook.result.sort(function (a, b){
+          var aSeq = parseInt(a._doc.sequence);
+          var bSeq = parseInt(b._doc.sequence);
+          return aSeq - bSeq;
+      });
+      
+  },
+  get(hook) {},
   create: [],
   update: [],
   patch: [],
